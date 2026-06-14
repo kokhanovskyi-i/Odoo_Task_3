5ebd28b868ffaf1f4b1802205aa55d6e7dc781bd
kokhanovskyi-i <jobseeker042011@gmail.com>
2026-06-15 02:01:23 +0300

    RUFF

--- models/hr_hospital_patient.py
+++ models/hr_hospital_patient.py
@@ -6,35 +6,35 @@ _logger = logging.getLogger(__name__)
 
 
 class HrHospitalPatient(models.Model):
-    _name = "hr.hospital.patient"
-    _description = "Hospital patient"
-    _inherit = ["hospital.medic.info"]
+    _name = 'hr.hospital.patient'
+    _description = 'Hospital patient'
+    _inherit = ['hospital.medic.info']
 
     name = fields.Char(
-        string="Name",
+        string='Name',
         required=True,
     )
 
     email = fields.Char(
-        string="Email",
+        string='Email',
     )
 
     phone = fields.Char(
-        string="Phone",
+        string='Phone',
     )
 
     personal_doctor_id = fields.Many2one(
-        comodel_name="hr.hospital.doctor",
-        string="Personal Doctor",
+        comodel_name='hr.hospital.doctor',
+        string='Personal Doctor',
     )
 
     doctor_history_ids = fields.One2many(
-        comodel_name="hospital.doctor.history",
-        inverse_name="patient_id",
-        string="Personal Doctor History",
+        comodel_name='hospital.doctor.history',
+        inverse_name='patient_id',
+        string='Personal Doctor History',
     )
 
     insurance_policy_number = fields.Char(
-        string="Insurance Policy Number",
+        string='Insurance Policy Number',
         size=20,
     )
